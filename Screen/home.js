7dfb8780b3b76dd4b46b522d7bfd82d7ea0536a2
Karumine <120722513+Karumine@users.noreply.github.com>
2023-02-20 16:37:02 +0700

    16.36 20/2/2566
    หน้า home ผู้ใช้

--- Screen/home.js
+++ Screen/home.js
@@ -1,12 +1,20 @@
 import * as React from 'react';
-import { View, Text, StyleSheet, StatusBar, Dimensions, TextInput, Image, FlatList, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, StatusBar, TouchableWithoutFeedback, Keyboard, Dimensions, SafeAreaView, TextInput, Image, FlatList, TouchableOpacity, ScrollView } from 'react-native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import * as Animatable from 'react-native-animatable';
 import { AntDesign, Ionicons, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
-import { firebase } from './components/SignUp'
+import { firebase } from './components/SignUp';
 import { useFocusEffect } from '@react-navigation/native';
-import banner from '../../ProjectGood/assets/banner.jpg'
-
+import banner from '../../ProjectGood/assets/banner.jpg';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+const COLOR = {
+    white: '#FFF',
+    dark: '#000',
+    primary: '#FF0000',
+    secondary: '#fedac5',
+    light: '#E5E5E5',
+    grey: '#908e8c',
+};
 
 const Tab = createMaterialBottomTabNavigator();
 const { height } = Dimensions.get("screen");
@@ -23,17 +31,37 @@ const styles = StyleSheet.create({
         height: 100,
     },
     header: {
+        marginTop: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20
+    },
+    inputContainer: {
         flex: 1,
-        justifyContent: 'flex-start',
-        paddingBottom: 50,
-
-        borderTopRightRadius: 30,
+        height: 50,
+        borderRadius: 10,
+        flexDirection: 'row',
+        backgroundColor: COLOR.light,
+        alignItems: 'center',
+        paddingHorizontal: 20,
+    },
+    sortBth: {
+        width: 50,
+        height: 50,
+        marginLeft: 10,
+        backgroundColor: COLOR.primary,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    categoriesListContainer: {
+        paddingVertical:30,
+        alignItems:'center',
+        paddingHorizontal:20,
     },
     footer: {
         flex: 1,
         backgroundColor: '#fff',
-        paddingVertical: 30,
-        paddingHorizontal: 30
     },
     text_header: {
         color: '#fff',
@@ -74,43 +102,62 @@ const styles = StyleSheet.create({
 });
 
 export default function HomeScreen({ navigation }) {
+    const [selectedCategoryIndex, setSelectedCategoryIndex] = React.useState(0);
+
+    const ListCategaries = () => {
+        return (
+            <ScrollView
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                contextContainerStyle={styles.categoriesListContainer}>
+                    
+                </ScrollView>
+        );
+    };
     return (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <SafeAreaView style={{ flex: 1, backgroundColor: COLOR.white }}>
+                <StatusBar backgroundColor='#FF0000' barStyle="light-content" />
+                <Animatable.View
+                    animation="fadeInUpBig"
+                    style={styles.footer}>
 
-        <View style={styles.container}>
+                    <View style={styles.header}>
+                        <View>
+                            <View style={{ flexDirection: 'row' }}>
+                                <Text style={{ fontSize: 28 }}>Hello,</Text>
+                                <Text style={{ fontSize: 28, fontWeight: 'bold', marginLeft: 10 }}>
+                                    Karumine
+                                </Text>
+                            </View>
+                            <Text style={{ marginTop: 5, fontSize: 22, color: COLOR.grey }}>
+                                What do you want today
+                            </Text>
+                        </View>
 
-            <StatusBar backgroundColor='#FF0000' barStyle="light-content" />
-
-            <Animatable.View
-                animation="fadeInUpBig"
-                style={styles.footer}
-            >
-                <View style={{ alignItems: 'flex-end' }}>
-
-
-                    <TouchableOpacity onPress={() => navigation.navigate('AccountUserScreen')}>
-                        <MaterialIcons name="account-circle" size={60} color="red" />
-                    </TouchableOpacity>
-                </View>
-
-                <Text style={{ marginTop: 60, fontSize: 30 }}>Restaurant</Text>
-
-                <View style={{ flexDirection: 'row' }}>
-                    <ScrollView
-                        horizontal={true}
-                    >
-                        <TouchableOpacity style={{ borderWidth: 1, backgroundColor: '#FF0000', marginVertical: 50, padding: 20, marginRight: 40, borderRadius: 100 }}>
-                            <MaterialCommunityIcons name="table-chair" size={40} color="black" />
+                        <TouchableOpacity onPress={() => navigation.navigate('AccountUserScreen')}>
+                            <MaterialIcons name="account-circle" size={60} color="red" />
                         </TouchableOpacity>
-                        <TouchableOpacity style={{ borderWidth: 1, backgroundColor: '#FF0000', marginVertical: 50, marginRight: 40, padding: 20, borderRadius: 100 }}>
-                            <MaterialIcons name="delivery-dining" size={40} color="black" />
-                        </TouchableOpacity>
-                    </ScrollView>
-                </View>
+                    </View>
+                    <View style={{
+                        marginTop: 40,
+                        flexDirection: 'row',
+                        paddingHorizontal: 20,
+                    }}>
+                        <View style={styles.inputContainer}>
+                            <Icon name="search" size={28} />
+                            <TextInput
+                                style={{ flex: 1, fontSize: 18 }}
+                                placeholder="Search for Restaurant"
+                            />
+                        </View>
+                        <View style={styles.sortBth}>
+                            <Icon name="tune" size={28} color={COLOR.white} />
+                        </View>
+                    </View>
 
-
-            </Animatable.View>
-
-        </View>
-
+                </Animatable.View>
+            </SafeAreaView>
+        </TouchableWithoutFeedback>
     );
 }
